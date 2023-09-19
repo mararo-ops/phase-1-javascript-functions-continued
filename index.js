a7d3e1011f2de1770251138bb92213e15d15ea29
mararo-ops <danielmararo1@gmail.com>
2023-09-20 00:47:36 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+
+function saturdayFun(money ="roller-skate") {
+    return(`This Saturday, I want to ${money}!`);
+}
+console.log (saturdayFun());
+const mondayWork = function (mula = "go to the office") {
+    return (`This Monday, I will ${mula}.`)
+}
+mondayWork("work from home");
+
+function wrapAdjective(love) {
+    return function(hate) {
+      return `You are ${love}${hate}${love}!`;
+    };
+  }
+  
+  let result = wrapAdjective('*');
+  let emphatic = result("a hard worker");
+  console.log(emphatic);
